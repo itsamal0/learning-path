1011c298d51f1e3e8ce717b92ae5f53ddf83bbbe
itsamal0 <amal@itsamal.com>
2025-08-31 14:43:09 +0300

    Add Problem44 solution for course07

---
+++ course07-algorithms-level-3/problems/problem44.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include "../helpers/helperFunctions.h"
+using namespace std;
+
+/*
+==================================================
+   Problem: Remove all punctuation characters 
+            from a string using a custom function.
+   From   : Programming Advices – Course 7
+   Date   : 2025-08-31
+==================================================
+*/
+
+int main() {
+    // Read original text from user
+    string originalText = helperFunctions::readString("\nPlease enter your text:");
+
+    // Show original string
+    cout << "\nOriginal string:\n" << originalText << endl;
+
+    // Remove punctuations using helperFunctions
+    cout << "\n:Punctuation removed:\n" 
+         << helperFunctions::removePunctuationsFromString(originalText) << endl;
+
+    // Wait for user input before closing
+    system("pause>0");
+}
