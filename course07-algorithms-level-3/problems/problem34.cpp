2c23817cb51729a794a05f4e57e4a7fb62642d71
itsamal0 <amal@itsamal.com>
2025-08-31 12:14:25 +0300

    Add Problem34 solution for course07

---
+++ course07-algorithms-level-3/problems/problem34.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include "../helpers/helperFunctions.h"
+using namespace std;
+
+/*
+==================================================
+   Problem: Write a program to print all vowels 
+            in a string.
+   From   : Programming Advices – Course 7
+   Date   : 2025-08-31
+==================================================
+*/
+
+// Check if a character is a vowel
+bool isVowel(char letter) {
+    letter = tolower(letter);
+    return (letter == 'a' || letter == 'e' || letter == 'i' || letter == 'o' || letter == 'u');
+}
+
+// Print all vowels in a string
+void printVowelsLetters(string str) {
+    cout << "\nVowels in string are : ";
+    for (int i = 0; i < str.length(); i++) {
+        if (isVowel(str[i])) {
+            cout << str[i] << "   ";
+        }
+    }
+}
+
+int main() {
+    // Read string from user using helperFunctions
+    string str = helperFunctions::readString("Please enter your string ?");
+    printVowelsLetters(str);
+
+    // Wait for user input before closing the console
+    system("pause>0");
+}
