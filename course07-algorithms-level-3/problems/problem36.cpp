c5f04ce2186cd6abe65c11e992c10c754b8fa5e0
itsamal0 <amal@itsamal.com>
2025-09-08 11:31:45 +0300

    Problem 36: Update to use io_utils library

--- course07-algorithms-level-3/problems/problem36.cpp
+++ course07-algorithms-level-3/problems/problem36.cpp
@@ -1,17 +1,17 @@
 #include <iostream>
-#include "../helpers/helperFunctions.h"
+#include "../helpers/io_utils.h"
 using namespace std;
 
 /*
 ==================================================
-   Problem: Write a program to count the number 
-            of words in a string.
-   From   : Programming Advices – Course 7
-   Date   : 2025-08-31
+   Problem: Write a program to count the number     
+            of words in a string.    
+   From   : Programming Advices – Course 7    
+   Date   : 2025-08-31    
 ==================================================
 */
 
-// Count the number of words in a string
+// Count the number of words in a string    
 short countEachWordInString(string str) {
     string delim = " ";
     short pos = 0, countWords = 0;
@@ -35,9 +35,9 @@ short countEachWordInString(string str) {
 }
 
 int main() {
-    // Read string from user using helperFunctions
-    string str = helperFunctions::readString("Please enter your string ?");
-    cout << "\nThe number of words in string is: " 
+    // Read string from user using io_utils
+    string str = io_utils::readString("Please enter your string ?");
+    cout << "\nThe number of words in string is: "
          << countEachWordInString(str);
 
     // Wait for user input before closing the console
