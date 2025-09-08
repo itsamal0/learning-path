4a1abc8f34ae69329eff9a2145154122e3cc2990
itsamal0 <amal@itsamal.com>
2025-09-08 07:08:23 +0300

    Create enum for main menu options

---
+++ course07-algorithms-level-3/projects/bank_system/src/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+enum enMainMenuOptions {
+    SHOW_CLIENTS = 1,
+    ADD_CLIENT = 2,
+    DELETE_CLIENT = 3,
+    UPDATE_CLIENT = 4,
+    FIND_CLIENT = 5,
+    EXIT_PROGRAM = 6
+};
+
+
+int main() {
+    return 0;
+}
