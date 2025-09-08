ebfee3bbfca8462776e3df02e2c9cdf28f6ba012
itsamal0 <amal@itsamal.com>
2025-09-08 07:17:34 +0300

    Add function to show the main menu

--- course07-algorithms-level-3/projects/bank_system/src/main.cpp
+++ course07-algorithms-level-3/projects/bank_system/src/main.cpp
@@ -10,6 +10,19 @@ enum enMainMenuOptions {
     EXIT_PROGRAM = 6
 };
 
+void showMainMenu() {
+    cout << "=================================\n";
+    cout << "          Bank System\n";
+    cout << "=================================\n";
+    cout << "[1] Show Clients List\n";
+    cout << "[2] Add Client\n";
+    cout << "[3] Delete Client\n";
+    cout << "[4] Update Client Info\n";
+    cout << "[5] Find Client\n";
+    cout << "[6] Exit\n";
+    cout << "=================================\n";
+}
+
 
 int main() {
     return 0;
