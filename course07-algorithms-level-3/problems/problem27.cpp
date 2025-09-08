539499dc55183c1dce2582281b7231beca35dcf6
itsamal0 <amal@itsamal.com>
2025-09-08 10:26:02 +0300

    Problem 27: Update code to use io_utils and char_utils libraries

--- course07-algorithms-level-3/problems/problem27.cpp
+++ course07-algorithms-level-3/problems/problem27.cpp
@@ -1,23 +1,22 @@
 #include <iostream>
-#include "../helpers/helperFunctions.h"
+#include "../helpers/io_utils.h"
+#include "../helpers/char_utils.h"
 using namespace std;
 
 /*
 ==================================================
-   Problem: Write a program to read a character and then 
-	    invert it's case.
+   Problem: Read a character and invert its case
    From   : Programming Advices – Course 7
    Date   : 2025-08-31
 ==================================================
 */
 
 int main() {
-    // Read a character from user using helperFunctions
-    char c = helperFunctions::readLetter("Please enter a character ?");
+    // Read a character from user using io_utils
+    char c = io_utils::readLetter("Please enter a character ? ");
 
     cout << "\nCharacter after inverting case:\n";
-    cout << helperFunctions::inverLetterCase(c);
-
+    cout << char_utils::invertLetterCase(c);
 
     // Wait for user input before closing the console
     system("pause>0");
