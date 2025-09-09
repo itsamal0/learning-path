cd519559025acde17f43650d9e9463df6db43e30
itsamal0 <amal@itsamal.com>
2025-09-09 09:04:12 +0300

    Add client management module with shared constants

---
+++ course07-algorithms-level-3/projects/bank_system/include/bank_system/client_management.h
@@ -0,0 +1,6 @@
+#pragma once
+#include <string>
+
+namespace client_management {
+    const std::string clientsFileName = "clients.txt";
+}
---
+++ course07-algorithms-level-3/projects/bank_system/src/client_management.cpp
@@ -0,0 +1,5 @@
+#include "../include/bank_system/client_management.h"
+
+namespace client_management {
+
+}
