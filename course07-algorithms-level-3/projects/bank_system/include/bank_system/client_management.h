6c677a75f601e0443e95d7be6a86c079da4a7930
itsamal0 <amal@itsamal.com>
2025-09-10 13:16:40 +0300

    Add updateClient logic and include in client_management header

--- course07-algorithms-level-3/projects/bank_system/include/bank_system/client_management.h
+++ course07-algorithms-level-3/projects/bank_system/include/bank_system/client_management.h
@@ -2,6 +2,7 @@
 #include "add_client.h"
 #include "show_clients.h"
 #include "delete_client.h"
+#include "update_client.h"
 #include <string>
 
 namespace client_management {
---
+++ course07-algorithms-level-3/projects/bank_system/src/update_client.cpp
@@ -0,0 +1,57 @@
+#include "../include/bank_system/client_utils.h"
+#include "../include/bank_system/client_management.h"
+#include "../../../helpers/io_utils.h"
+#include <iostream>
+#include <fstream>
+#include <limits>
+using namespace std;
+
+
+namespace client_management {
+
+    // Update a client by account number
+    void updateClient() {
+        stClient client;
+        char answer = 'n';
+
+        vector<stClient> vClients = loadClientsDataFromFile(clientsFileName);
+            
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        string accountNumber = io_utils::readString("Please enter account number ? ");
+
+        if (findClientByAccountNumber(accountNumber, vClients, client)) {
+            displayClientRecord(client);
+
+            cout << "\n\nAre you sure you want to update this client? (y/n): ";
+            cin >> answer;
+
+            if (toupper(answer) == 'Y') {
+                // Clear input buffer
+                cin.ignore(1000, '\n');
+
+                stClient updatedClient;
+                updatedClient.accountNumber = accountNumber;
+                updatedClient.pinCode = io_utils::readString("\nEnter new pin code: ");
+                updatedClient.name = io_utils::readString("\nEnter new name: ");
+                updatedClient.phone = io_utils::readString("\nEnter new phone: ");
+                updatedClient.accountBalance = io_utils::readPositiveNumber("\nEnter new account balance: ");
+
+                // Update the client in the vector
+                for (stClient& c : vClients) {
+                    if (c.accountNumber == accountNumber) {
+                        c = updatedClient;
+                        break;
+                    }
+                }
+
+                // Save updated clients to file
+                saveClientsDataToFile(clientsFileName, vClients);
+                cout << "\n\nClient updated successfully.\n";
+                return;
+            }
+        } else {
+            cout << "\nClient with account number (" << accountNumber << ") is NOT found!";
+        }
+    }
+}
