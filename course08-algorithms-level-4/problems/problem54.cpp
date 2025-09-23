1a1596fdd1a9e8dd2df70aa816fda45a234800dc
itsamal0 <amal@itsamal.com>
2025-09-23 14:31:47 +0300

    feat(course08): Add problem54 solution

---
+++ course08-algorithms-level-4/problems/problem54.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include "../../helpers/io_utils.h"
+#include "../../helpers/date_utils.h"
+using namespace std;
+
+/*
+==================================================
+Problem: Write a program to read Vacation Period 
+         DateFrom and DateTo and make a function 
+         to calculate the actual vacation days.
+         Note: Weekends are excluded.
+From   : Programming Advices – Course 8
+Date   : 2025-09-23
+==================================================
+*/
+
+
+// Wait for user to press Enter before exiting
+void waitForEnter(string message) {
+    cout << "\n" << message;
+    cin.ignore();
+    cin.get();
+}
+
+short calculateVacationDays(stDate DateFrom, stDate DateTo) {
+    short daysCount = 0;
+
+    while (true) {
+        if (date_utils::isBusinessDay(date_utils::getDayOfWeekOrder(DateFrom))) {
+            daysCount++;
+        }
+
+        if (date_utils::isDate1EqualDate2(DateFrom, DateTo)) {
+            break;
+        }
+
+        DateFrom = date_utils::increaseDateByOneDay(DateFrom);
+    }
+
+    return daysCount;
+}
+
+
+int main() {
+    cout << "\nVacation starts:";
+    stDate DateFrom = date_utils::readFullDate();
+    short dayOfWeekOrderOfDateFrom = date_utils::getDayOfWeekOrder(DateFrom);
+
+    cout << "\nVacation ends:";
+    stDate DateTo = date_utils::readFullDate();
+    short dayOfWeekOrderOfDateTo = date_utils::getDayOfWeekOrder(DateTo);
+
+    cout << "\nVacation from: " << date_utils::getDayShortName(dayOfWeekOrderOfDateFrom);
+    cout << "\nVacation to: " << date_utils::getDayShortName(dayOfWeekOrderOfDateTo);
+
+    cout << "\n\nActucal vacation days is: " << calculateVacationDays(DateFrom, DateTo) << endl;
+
+    waitForEnter("Press Enter to exit...");
+
+    return 0;
+}
